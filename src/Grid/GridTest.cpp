4a98e9b58967cdd7f6659cd7bfcc0e93615aa052
LiamPattinson <liampattinson@gmail.com>
2017-01-12 19:03:46 +0000

    Updated GridTest.cpp
    Updated GridTest to comply with new changes. Used to test new
    Grid styles (NoGhosts, EdgeCentred, etc). They actually seem
    to work. Syntax to use them is fairly awful though. Will have
    to rely heavily on typedefs to use the new changes.

--- src/Grid/GridTest.cpp
+++ src/Grid/GridTest.cpp
@@ -1,46 +1,46 @@
 // GridTest.cpp
 //
-// A simple program designed to test Grid.hpp
+// A simple program designed to test Grids
 
 #define MAXWELL 1
+#define MY_DIM 2
 
 #include <cstdlib>
 #include <iostream>
-#include "GridImplementation.hpp"
-#include "StateGridCartesianCentred.hpp"
-#include "FluxGridCartesianCentred.hpp"
-#include "LevelSetGridCartesianCentred.hpp"
-#include "CutCellGrids.hpp"
+#include "CartesianSubGrid.hpp"
+#include "SubGrids/StateGrid.hpp"
+#include "SubGrids/FluxGrid.hpp"
+#include "SubGrids/MaterialGrid.hpp"
 
 using namespace FVlite;
 
 // Define grid
 
-#define MY_DIM 2
+typedef CartesianSubGrid<MY_DIM,CellCentred,WithGhosts> PrimaryGrid;
+typedef CartesianSubGrid<MY_DIM,EdgeCentred<DIM_X>,NoGhosts> SecondaryGrid;
 
-typedef CartesianGridImplementation<
+typedef CartesianGrid<
     MY_DIM,
-    StateGridCartesianCentred<MY_DIM>,
-    FluxGridCartesianCentred<MY_DIM>,
-    LevelSetGridCartesianCentred<MY_DIM>,
-    CutCellBoundaryStateGrid<MY_DIM>,
-    CutCellGeometryGrid<MY_DIM>
+    StateGrid<PrimaryGrid>,
+    MaterialGrid<Material,PrimaryGrid>,
+    FluxGrid<SecondaryGrid>
 > Grid;
 
 // Test it out
 
 int main(void){
 
-    double L[2] = { 10.0, 10.0};
-    unsigned int N[2] = {10,10};
+    double L[2] = { 10.0, 5.0};
+    unsigned int N[2] = {10,5};
 
-    Grid grid( L, N, 1);
+    Grid grid( L, N, 2);
 
+    Material mat(1.0,1.0);
     StateVector vec1(1.0,1.0,1.0,1.0,1.0,1.0);    
     StateVector vec2(1.0,0.0,0.0,1.0,0.0,0.0);    
     grid.state(3,4) = vec1;
     FluxVector flux;
-    flux.set(vec1,'x');
+    flux.set(vec1,mat,'x');
     grid.flux(1,5) = flux;
 
     grid.state(5,2) = vec2;
@@ -53,8 +53,46 @@ int main(void){
               << vec3[4] << ' '
               << vec3[5] << std::endl;
 
-    std::cout << grid.position(DIM_X,5) << std::endl;
-    std::cout << grid.position(DIM_Y,5) << std::endl;
+    std::cout << "StateGrid:" << std::endl;
+    for( int jj=0; jj<grid.PrimaryGrid::full_elements(DIM_Y); jj++){
+        for( int ii=0; ii<grid.PrimaryGrid::full_elements(DIM_X); ii++){
+            std::cout << "(" ;
+            std::cout <<  grid.PrimaryGrid::position(DIM_X,ii) << ",";
+            std::cout <<  grid.PrimaryGrid::position(DIM_Y,jj) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "FluxGrid:" << std::endl;
+    for( int jj=0; jj<grid.SecondaryGrid::full_elements(DIM_Y); jj++){
+        for( int ii=0; ii<grid.SecondaryGrid::full_elements(DIM_X); ii++){
+            std::cout << "(" ;
+            std::cout <<  grid.SecondaryGrid::position(DIM_X,ii) << ",";
+            std::cout <<  grid.SecondaryGrid::position(DIM_Y,jj) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nUsing start/end:" << std::endl;
+    std::cout << "StateGrid:" << std::endl;
+    for( int jj=grid.PrimaryGrid::start(DIM_Y); jj<grid.PrimaryGrid::end(DIM_Y); jj++){
+        for( int ii=grid.PrimaryGrid::start(DIM_X); ii<grid.PrimaryGrid::end(DIM_X); ii++){
+            std::cout << "(" ;
+            std::cout <<  grid.PrimaryGrid::position(DIM_X,ii) << ",";
+            std::cout <<  grid.PrimaryGrid::position(DIM_Y,jj) << ") ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "FluxGrid:" << std::endl;
+    for( int jj=grid.SecondaryGrid::start(DIM_Y); jj<grid.SecondaryGrid::end(DIM_Y); jj++){
+        for( int ii=grid.SecondaryGrid::start(DIM_X); ii<grid.SecondaryGrid::end(DIM_X); ii++){
+            std::cout << "(" ;
+            std::cout <<  grid.SecondaryGrid::position(DIM_X,ii) << ",";
+            std::cout <<  grid.SecondaryGrid::position(DIM_Y,jj) << ") ";
+        }
+        std::cout << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
